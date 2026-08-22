d657b9fd647c313c34ba9fc1c259e95f8e972049
juanmotoya-cloud <juan.motoya@alunos.ifsuldeminas.edu.br>
2026-08-22 18:34:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,31 +11,23 @@ app.get('/api/courses/:id', (req, res) => {
     const id = Number(req.params.id);
 
     if (Number.isNaN(id)) {
-        res.status(400);
-        res.setHeader('Content-Type', 'application/json');
-
-        return res.end(JSON.stringify({
+        return res.status(400).json({
             success: false,
             error: 'ID do curso inválido'
-        }));
+        });
     }
 
     const course = courses.find(course => course.id === id);
 
     if (!course) {
-        res.status(404);
-        res.setHeader('Content-Type', 'application/json');
-
-        return res.end(JSON.stringify({
+        return res.status(404).json({
             success: false,
             error: 'Curso não encontrado'
-        }));
+        });
     }
 
-    res.setHeader('Content-Type', 'application/json');
-
-    return res.end(JSON.stringify({
+    return res.status(200).json({
         success: true,
-        course: course
-    }));
+        data: course
+    });
 });
